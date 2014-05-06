2b2f9cedaf375a470ada3f129f132fd46a92318e
Pezhvak <pezhvakimv@gmail.com>
2014-05-06 15:21:28 +0430

    changes

--- www/js/index.js
+++ www/js/index.js
@@ -33,6 +33,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-
+        $('.app').html('BNKH');
     }
 };
